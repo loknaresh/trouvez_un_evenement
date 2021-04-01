668567bdd0fce4c4e26895a168d047351d81a306
Mohamed_Raissa <raissamze@gmail.com>
2021-04-01 18:31:40 +0200

    mise à jour connexion.php

--- connexion.php
+++ connexion.php
@@ -1,20 +1,46 @@
-<?php
- session_start();
- ?>
+
 <!DOCTYPE HTML>
 <HTML>
     <HEAD>
         <TITLE>connexion</TITLE>
-       <!--<link rel="stylesheet" type="text/css" href="style.css"/>-->
-        <link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" />
-    <!--  fichier 6 / -->
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <style>
+            .div-droite
+            {
+	            width: 50%;
+                border:2px solid red;
+                background: #55d6aa;
+                margin: 60px auto 30px auto;
+                //flex-grow: 1;
+                padding: 40px;
+                margin-bootom:30px;
+
+            }
+            form
+            {
+                margin-top: 10%;
+                margin-left:30%;
+            }
+        </style>
+     <!-- fichier 6 / -->
     </HEAD>
     <body>
-        <form action="connecter.php" method="POST" autocomplete="off">
-        <p>Adresse email :  <input type="email" name="mail" value=""/></p>
-        <p>Mot de passe :  <input required type="password" name="mot_de_passe" value=""/></p>
-        <p><input type="submit" value="Valider"></p>
-        </form>
-        <a href="page_inscription.php">Inscription</a>
+        <?php
+        $page="connexion";
+		 require ('header.php');
+		?> 
+        <div class="div-droite"> 
+            <a href="index.php"><img src="logo2.png" alt="logo" class="logo"></a>
+            <a href="index.php"><img src="logo1.png" alt="logo" class="logo1"></a>
+        
+            <form action="connecter.php" method="POST" autocomplete="off">
+            <p>Adresse email :  <input type="email" name="mail" value=""/></p>
+            <p>Mot de passe :  <input required type="password" name="mot_de_passe" value=""/></p>
+            <p><input type="submit" value="Valider"></p>
+            </form>  
+        </div>
+        <?php
+		require ('footer.php');
+		?> 
     </body>
 </html>
