3f859c808068f58748fc53268cb12b442917f6fd
Mohamed_Raissa <raissamze@gmail.com>
2021-04-01 18:32:16 +0200

    mise à jour contacter.php

---
+++ contacter.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>version 3 </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <!--  fichier 8 / -->
+        
+        <style type="text/css">
+        body{
+            background-color: #55d6aa;;
+        }
+      
+
+        </style>
+    </head>
+    <body>
+        <header>
+		    <div class="container">
+                <p>Bonjour</p>
+            </div>
+    	</header>
+</body>
+</html>
