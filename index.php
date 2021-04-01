09e764c6d957f2a67e1fed4e180549fccb49539e
Mohamed_Raissa <raissamze@gmail.com>
2021-04-01 18:39:35 +0200

    mise à jopur de index.php

--- index.php
+++ index.php
@@ -1,79 +1,70 @@
-<?php
-session_start();
-?>
  
 <!DOCTYPE html>
 <html>
-<head>
-	<title>version 3 </title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<!-- Mon premier fichier () / -->
-</head>
-<body>
-	<?php 
-		require('bd.php');
-		$connexion=getBD();
-		$rep = $connexion ->query('select * from evenement');
+	<head>
+		<title>version 3 </title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<!-- Mon premier fichier () / -->
+	</head>
+	<body>
+ 		<?php
+		 $page="ndex";
+		 require ('header.php');
+		?> 
+		<div class="content-container">
+			<form>
+				<div class="div-gauche">
+					
+					<p><input type="text" placeholder="saisir votre évenement..." name="search" autocomplete="off"></p>
+					<p><input type="text" placeholder=" adresse..." name="search" autocomplete="off"></p>
+					<p><input type="text" placeholder="date/heure debut..." name="search" autocomplete="off"></p>
+					<p><input type="text" placeholder="date/heure fin..." name="search" autocomplete="off"></p>
+					<p><input type="text" placeholder="tarif... " name="search" autocomplete="off"></p>
+					<input type="submit" value="Rechercher">
+					<div>
+					<p><input type="reset" value="Rénitialiser"></p>
+					</div>
+			</form>
+				
+			</div>
+			<div class="div-droite">
+				<p>AFFICHER LES EVENEMENTS</p>
 
-	
-	?>
-	
+				<?php
+					$rep = getBD()->query('select * from evenement where adresse_id=1');
+					var_dump($rep);
+					//exit();
+					
+					/*
+					echo "<table class='table'>
+					<tr>
+						<th> Identifiant</th>
+						<th>uidentifiant utilisateur</th>
+						<th>Description</th>
+						<th>Tarif</th>
+						<th>Titre</th>
+					</tr>";
 
-	<header>
-		<div class="container">
-			<a href="index.php"><img src="logo2.png" alt="logo" class="logo"></a>
-			<nav>
-				<ul>
-					<li><a href="#">Rechercher</a></li>
-					<li><a href="#">Organiser</a></li>
-					<li><a href="#">Consulter Carte Geographique</a></li>
-					<li><a href="#">Consulter Calendrier</a></li>
-					<li><a href="#">Nous Contacter</a></li>
-					<?php
-						if (isset($_SESSION['utilisateur']))
-						{
-							echo"<br>";
-							echo"  Bonjour et Bienvenue ";
-							echo " ";
-							echo $_SESSION['utilisateur']["prenom"];
-							echo " ";
-							echo $_SESSION['utilisateur']["nom"];
-							echo " ! ";
-							echo"<br>";
-							echo'<a href="deconnexion.php">Deconnexion</a>';
-						}
-						else
-						{
-							echo "<li><a href='page_inscription.php'> S'inscrire</a></li>";
-							echo "<li><a href='connexion.php'>Se connecter</a></li>";
-						}
-						?>
-				</ul>
-			</nav>
+					while ($ligne = $rep ->fetch())
+					{	
+						echo "<tr>";
+						echo "<td>".$ligne["evenement_id"]."</td>";
+						echo "<td>  ".$ligne['utilisateur_id']."</td>";
+						echo "<td>".$ligne['description']."</td>";
+						echo "<td>".$ligne["tarif"]."</td>";
+						echo "<td>".$ligne["titre"]."</td>";
+						echo "</tr>";
+					}
+					echo "</table >";
+					$rep ->closeCursor(); */
+					
+				
+				?>
+			</div>
 		</div>
-	</header>
-
-	<br>
-
-	<div class="content-container">
-
-		<div class="div-gauche">
-			<input type="text" placeholder="Rechercher..." name="search" autocomplete="off">
-			<br>
-			<br>
-			<br>
-			<p>...</p>
-			<br>
-			<br>
-			<br>
-		</div>
-		<div class="div-droite">
-			<p>AFFICHER LES EVENEMENTS</p>
-		</div>
-
-	</div>
-
-
-</body>
+		<?php 
+		require ('footer.php');
+		?> 
+	</body>
 </html>
