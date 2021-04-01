17587664d8f34318647ad2f91970a3e9fea10b94
Mohamed_Raissa <raissamze@gmail.com>
2021-04-01 18:38:32 +0200

    ajout de footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+<div class="footer-dark">	
+    <footer>
+    
+        <div class="bloc">
+            
+            <h3>Information pratique</h3>	
+            <ul>
+                <li><a href="#">Deposer une annonce</a></li>
+                <li><a href="#">D</a></li>
+            </ul>
+        </div>
+        <div class="bloc">
+            <h3>En savoir +</h3>
+            <ul>
+                <li><a href="#">Qui sommes nous</a></li>
+                <li><a href="#">T</a></li>
+            </ul>
+        </div>
+        <div class="bloc">
+            <h3>Contact</h3>
+            <ul>
+                <li><a href="contacter.php">Nous Contacter</a></li>
+            </ul>
+        </div>
+
+    </footer>
+</div>
+ 
